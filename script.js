4557e8f82df95dd061ff1ee49b50a0f0c432df3a
shahzad akram <alyakram786@gmail.com>
2024-03-02 21:22:12 +0500

    making an inserting contacts

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-const invert =
- [
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {}
+const inventors = [
+  { first: "Albert ", last: "Einstein ", year: 1879, passed: 1955 },
+  { first: "Isaac ", last: "Newton ", year: 1643, passed: 1727 },
+  { first: "Galileo ", last: "Galelei ", year: 1564, passed: 1642 },
+  { first: "Marie ", last: "Curie ", year: 1867, passed: 1934 },
+  { first: "Johanees ", last: "Kepler ", year: 1571, passed: 1630 },
+  { first: "Nicolaus ", last: "Copernicus ", year: 1473, passed: 1543 },
+  { first: "Max ", last: " Plank", year: 1858, passed: 1947 },
 ];
